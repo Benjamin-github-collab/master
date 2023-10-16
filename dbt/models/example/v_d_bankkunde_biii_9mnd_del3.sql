03d1d9d0b6512a81d72ee72afc03517c10e842ea
Benjamin-github-collab <benjamin.leif.gjesdal@sparebank1.no>
2023-10-16 10:06:54 +0000

    changes to v_d_bankkunde_biii_del3

--- dbt/models/example/v_d_bankkunde_biii_9mnd_del3.sql
+++ dbt/models/example/v_d_bankkunde_biii_9mnd_del3.sql
@@ -1,38 +1,13 @@
-WITH RECURSIVE HierarchicalCTE AS (
-  SELECT
-    k.*,
-    1 AS n, -- Initial level
-    k.sk_bankkunde_biii_id AS top_sk,
-    k.sak_start_dato AS top_sak_start_dato,
-    k.sak_kilde_init AS top_sak_kilde_init,
-    k.overforing_arsak_init_kode AS top_overforing_arsak_init_kode,
-    CASE WHEN k2.sk_bankkunde_biii_id IS NULL THEN 1 ELSE 0 END AS er_siste_flagg
-  FROM
-    {{ ref('v_d_bankkunde_biii_9mnd') }} k
-  LEFT JOIN
-    {{ ref('v_d_bankkunde_biii_9mnd_del2') }} k2 ON k.sk_bankkunde_biii_id = k2.sk_bankkunde_biii_id
-  
-  UNION ALL
-  
-  SELECT
-    k.*,
-    h.n + 1 AS n,
-    h.top_sk,
-    h.top_sak_start_dato,
-    h.top_sak_kilde_init,
-    h.top_overforing_arsak_init_kode,
-    CASE WHEN k.sk_bankkunde_biii_id IS NULL THEN 1 ELSE 0 END AS er_siste_flagg
-  FROM
-    {{ ref('v_d_bankkunde_biii_9mnd') }} k
-  JOIN
-    HierarchicalCTE h
-  ON
-    k.rk_bankkunde_id = h.rk_bankkunde_id
-    AND k.kundesak_nr + 1 = h.kundesak_nr
-    AND months_between(k.sak_start_dato, h.sak_avsluttet_dato) <= 9
-)
-
-select * from HierarchicalCTE
-
+SELECT level, 
+  connect_by_root sk_bankkunde_biii_id as parent_sk_bankkunde_biii_id, 
+  connect_by_root rk_bankkunde_id as parent_rk_bankkunde_id, 
+  connect_by_root sak_start_dato as parent_sak_start_dato,
+  SYS_CONNECT_BY_PATH(kundesak_nr, ' -> '), 
+  k.*
+  FROM {{ ref('v_d_bankkunde_biii_9mnd') }} k
+    START WITH sk_bankkunde_biii_id in (select sk_bankkunde_biii_id from {{ ref('v_d_bankkunde_biii_9mnd_del2') }} )
+    CONNECT BY prior k.rk_bankkunde_id = k.rk_bankkunde_id
+           and prior k.kundesak_nr + 1 = k.kundesak_nr
+           and months_between(k.sak_start_dato, prior k.sak_avsluttet_dato) <= 9;
 
 
